e13412506869adb480bc3544cbe7854dd3e2d861
albarracin-jorge <albarracinj.dev@gmail.com>
2024-02-10 14:48:53 -0300

    [fix] bash script

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@ docker kill jalbarracin
 
 docker container rm jalbarracin
 
-git pull origin main
+git pull origin main && \
 
-npm run build
+npm run build && \
 
-docker image rm nginx-jalbarracin
+docker image rm nginx-jalbarracin && \
 
-docker build -t nginx-jalbarracin .
+docker build -t nginx-jalbarracin . && \
 
 docker run -d -p 8080:80 --name jalbarracin nginx-jalbarracin
