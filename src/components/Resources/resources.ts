756a9b38de2c956028a3bbea99d7a1e3c09a55c7
albarracin-jorge <albarracinj.dev@gmail.com>
2024-02-18 22:16:18 -0300

    [add] resource item & refactor ListResource.astro

---
+++ src/components/Resources/resources.ts
@@ -0,0 +1,70 @@
+const ResourceList = [
+	{
+		title: "Squoosh",
+		urlResource: "https://squoosh.app/",
+		description: "Sitio web para optimizar imagenes reduciendo su tamaño sin perder calidad 📷.",
+		imageResource: "https://squoosh.app/c/favicon-c9cf50ef.ico",
+	},
+	{
+		title: "AdventJS",
+		urlResource: "https://adventjs.dev/",
+		description: "Sitio web para practicar catas, cada año en diciembre lanzan nuevos desafios para pulir resolución de problemas y algoritmos.",
+		imageResource: "https://adventjs.dev/favicon-96x96.png",
+	},
+	{
+		title: "Free Programming Books",
+		urlResource: "https://goalkicker.com/",
+		description: "Este sitio cuenta con muchos libros de estudio de múltiples lenguajes y tecnologías 📕. Sin embargo considero algunos libros en particular un poco antiguos o desactualizados.",
+		imageResource: "",
+	},
+	{
+		title: "Radix UI",
+		urlResource: "https://www.radix-ui.com/",
+		description: "Libreria UI para componentes en React.",
+		imageResource: "https://www.radix-ui.com/favicon.png",
+	},
+	{
+		title: "DB Diagram",
+		urlResource: "https://dbdiagram.io/",
+		description: "Herramienta para diagramar bases de datos relacionales 📈.",
+		imageResource: "https://cdn.holistics.io/logo-dbdiagram-notext.ico",
+	},
+	{
+		title: "Shadcn UI",
+		urlResource: "https://ui.shadcn.com/docs",
+		description: "Otra libería UI para componentes en React, también con soporte en Svelte ✨.",
+		imageResource: "https://ui.shadcn.com/favicon.ico",
+	},
+	{
+		title: "UIverse",
+		urlResource: "https://uiverse.io/",
+		description: "Sitio lleno de elementos UI para cualquier proyecto.",
+		imageResource: "https://uiverse.io/favicon-32x32.png",
+	},
+	{
+		title: "JSON Data AI",
+		urlResource: "https://www.jsondataai.com/",
+		description: "Este sitio web ayuda a generar contenido JSON en base a los requisitos solicitados, muy útil para generar mocks.",
+		imageResource: "https://www.jsondataai.com/favicon.ico",
+	},
+	{
+		title: "You might not need jQuery",
+		urlResource: "https://youmightnotneedjquery.com/",
+		description: "Sitio web con alternativas prácticas a jQuery 😮.",
+		imageResource: "",
+	},
+	{
+		title: "Bundle Phobia",
+		urlResource: "https://bundlephobia.com/",
+		description: "Encuentra paquetes npm y devuelve el peso de dicha libreria, útil para fines de performance.",
+		imageResource: "",
+	},
+	{
+		title: "Learn Testing by Google",
+		urlResource: "https://web.dev/learn/testing/welcome?hl=es-419",
+		description: "Curso introductorio al testing web: fundamentos, aspectos básicos y buenas prácticas.",
+		imageResource: "https://www.gstatic.com/devrel-devsite/prod/v5ba20c1e081870fd30b7c8ebfa8711369a575956c1f44323664285c05468c6a4/web/images/favicon.png",
+	},
+]
+
+export default ResourceList;
