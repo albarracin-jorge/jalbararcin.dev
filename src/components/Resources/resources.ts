b467ab050a72d703eb2b61fc9fd6d91e3ab531fb
Jorge Albarracin <albarracinj.dev@gmail.com>
2024-07-25 17:15:49 -0300

    [update] translate to english, replace color theme dark & light, fix responsive margins, update timeline

--- src/components/Resources/resources.ts
+++ src/components/Resources/resources.ts
@@ -1,70 +1,78 @@
+import adventLogo from "../../assets/js-logo.png";
+
 const ResourceList = [
 	{
 		title: "Squoosh",
 		urlResource: "https://squoosh.app/",
-		description: "Sitio web para optimizar imagenes reduciendo su tamaño sin perder calidad 📷.",
+		description: "Website to optimize images by reducing their size without losing quality 📷.",
 		imageResource: "https://squoosh.app/c/favicon-c9cf50ef.ico",
 	},
 	{
 		title: "AdventJS",
 		urlResource: "https://adventjs.dev/",
-		description: "Sitio web para practicar catas, cada año en diciembre lanzan nuevos desafios para pulir resolución de problemas y algoritmos.",
-		imageResource: "https://adventjs.dev/favicon-96x96.png",
+		description: "Website to practice coding challenges. Every December, they release new challenges to improve problem-solving skills and algorithms.",
+		imageResource: adventLogo,
 	},
 	{
 		title: "Free Programming Books",
 		urlResource: "https://goalkicker.com/",
-		description: "Este sitio cuenta con muchos libros de estudio de múltiples lenguajes y tecnologías 📕. Sin embargo considero algunos libros en particular un poco antiguos o desactualizados.",
+		description: "This website offers many study books for multiple languages and technologies 📕. However, some books may be outdated or not up to date.",
 		imageResource: "",
 	},
 	{
 		title: "Radix UI",
 		urlResource: "https://www.radix-ui.com/",
-		description: "Libreria UI para componentes en React.",
+		description: "UI library for React components.",
 		imageResource: "https://www.radix-ui.com/favicon.png",
 	},
 	{
 		title: "DB Diagram",
 		urlResource: "https://dbdiagram.io/",
-		description: "Herramienta para diagramar bases de datos relacionales 📈.",
+		description: "Tool for diagramming relational databases 📈.",
 		imageResource: "https://cdn.holistics.io/logo-dbdiagram-notext.ico",
 	},
 	{
 		title: "Shadcn UI",
 		urlResource: "https://ui.shadcn.com/docs",
-		description: "Otra libería UI para componentes en React, también con soporte en Svelte ✨.",
+		description: "Another UI library for React components, also with support for Svelte ✨.",
 		imageResource: "https://ui.shadcn.com/favicon.ico",
 	},
 	{
 		title: "UIverse",
 		urlResource: "https://uiverse.io/",
-		description: "Sitio lleno de elementos UI para cualquier proyecto.",
+		description: "Website filled with UI elements for any project.",
 		imageResource: "https://uiverse.io/favicon-32x32.png",
 	},
 	{
 		title: "JSON Data AI",
 		urlResource: "https://www.jsondataai.com/",
-		description: "Este sitio web ayuda a generar contenido JSON en base a los requisitos solicitados, muy útil para generar mocks.",
+		description: "This website helps generate JSON content based on the requested requirements, very useful for generating mocks.",
 		imageResource: "https://www.jsondataai.com/favicon.ico",
 	},
 	{
 		title: "You might not need jQuery",
 		urlResource: "https://youmightnotneedjquery.com/",
-		description: "Sitio web con alternativas prácticas a jQuery 😮.",
+		description: "Website with practical alternatives to jQuery 😮.",
 		imageResource: "",
 	},
 	{
 		title: "Bundle Phobia",
 		urlResource: "https://bundlephobia.com/",
-		description: "Encuentra paquetes npm y devuelve el peso de dicha libreria, útil para fines de performance.",
+		description: "Find npm packages and get their size, useful for performance purposes.",
 		imageResource: "",
 	},
 	{
 		title: "Learn Testing by Google",
 		urlResource: "https://web.dev/learn/testing/welcome?hl=es-419",
-		description: "Curso introductorio al testing web: fundamentos, aspectos básicos y buenas prácticas.",
+		description: "Introductory course on web testing: fundamentals, basics, and best practices.",
 		imageResource: "https://www.gstatic.com/devrel-devsite/prod/v5ba20c1e081870fd30b7c8ebfa8711369a575956c1f44323664285c05468c6a4/web/images/favicon.png",
 	},
+	{
+		title: 'CSS Layout Generator',
+		urlResource: 'https://layout.bradwoods.io/customize',
+		description: 'CSS layout generator with customizable options.',
+		imageResource: 'https://layout.bradwoods.io/apple-touch-icon.png',
+	}
 ]
 
 export default ResourceList;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				'header': '0 24px 24px 0 rgb(203 213 225 / var(--tw-bg-opacity))',
+				'dark-header': '0 24px 24px 0 rgb(2 6 23 / var(--tw-bg-opacity))',
+				'main-l': 'inset -48px 0 48px -12px rgb(226 232 240 / var(--tw-bg-opacity))',
+				'dark-main-l': 'inset -48px 0 48px -12px rgb(15 23 42 / var(--tw-bg-opacity))',
+				'main-r': 'inset 48px 0 48px -12px rgb(226 232 240 / var(--tw-bg-opacity))',
+				'dark-main-r': 'inset 48px 0 48px -12px rgb(15 23 42 / var(--tw-bg-opacity))',
+			},
+		},
 	},
 	plugins: [],
 }
